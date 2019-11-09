096f64dc92ccc948d85d4a443c8dde6ba290f9cb
Wills Blake <richwblake@gmail.com>
2019-11-09 01:08:20 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,8 +2,8 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name => "",
-        :age => 3
+        :name => "Lord Montague",
+        :age => 53
         },
       :matriarch => {
         :name => "",
