8551bdd583ff639cf2ed66e47a286c9352245c35
Wills Blake <richwblake@gmail.com>
2019-11-09 01:06:29 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -10,7 +10,9 @@ def third_challenge
         :age => 3
         },
       :hero => {
-        #your key/value pairs here
+        :name => ""
+        :age => = 3
+        :status = ""
         },
       :hero_friends => []
    }, 
